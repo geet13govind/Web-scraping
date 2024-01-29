c5986e10ab7d669b541cfd5258cee6ac6ce057f4
GeetGovind <geet13govind@gmail.com>
2024-01-29 23:00:13 +0530

    all commit

--- scraping and save file in text own.py
+++ scraping and save file in text own.py
@@ -6,21 +6,19 @@ def get_latest_python_articles(url,output_file):
 
     if response.status_code==200:
         soup = BeautifulSoup(response.text,'html.parser')
-        
-        file=open('Latest_articles.txt','w')
-        file.write('\n')
-        file.write('Latest articles in python section are: \n\n')
-        print('Latest articles in python section are: \n\n')
+  
 
         latest_articles=soup.select('.blog-widget li')
 
-        with open(output_file,'a',encoding='utf-8') as file:
+        with open(output_file,'w',encoding='utf-8') as file:
+            file.write('Latest articles in python section are: \n\n')
+            print('\nLatest articles in python section are: \n')
             for index, article in enumerate(latest_articles,1):
                 title = article.a.text.strip()
                 file.write(f'{index}. {title}\n')
                 print(f'{index}. {title}\n')
 
-        print(f"Article saved to : {output_file}")
+        print(f"Article saved to : {output_file}\n")
 
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
--- scraping by mam.py
+++ scraping by mam.py
@@ -1,28 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-def latest_python_articles(url):
+def upcoming_events(url):
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text,'html.parser')
-        latest_article = []
+        new_events = []
 
-        for article in soup.select('.blog-widget li'):
-            title = article.a.text.strip()
-            latest_article.append(title)
+        for events in soup.select('.event-widget.last li'):
+            title = events.a.text.strip()
+            new_events.append(title)
 
-        return latest_article
+        return new_events
     
     else:
         print(f'failed to retrieve the information.status code : {response.status_code}')
         return []
     
 if __name__ == "__main__":
-    python_articles = latest_python_articles("https://www.python.org/")
-    if python_articles is not None:
-        print('Latest articles in python section: ')
-        for index,article in enumerate(python_articles,1):
+    coming_events = upcoming_events("https://www.python.org/")
+    if coming_events is not None:
+        print('Upcoming events in python section: ')
+        for index,article in enumerate(coming_events,1):
             print(f'{index}. {article}')
+            
                   
 
     else:
